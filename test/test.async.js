8e7f7660051a09558941714f2b9af32de0442b59
Anthony Beaumont <dev.xan105@outlook.be>
2022-12-22 14:14:15 +0700

    update

--- test/test.async.js
+++ test/test.async.js
@@ -16,20 +16,10 @@ console.log(
   await XInput.promises.getStateEx()
 );
 
-//async queue ok
+//async queue
 console.log(0);
 XInput.promises.rumble()
 .then(()=>{ return XInput.promises.rumble(); })
 .then(()=>{ console.log(2); })
 .catch(console.error);
 console.log(1);
-
-//await XInput.promises.enable(false);await XInput.promises.rumble(); //err device not connected triggered->ok
-//await XInput.promises.setState(50,50,0);setTimeout(()=>{XInput.promises.enable(false)},500); //rumble interrupt after 500ms->ok
-//await XInput.promises.rumble({force: 50});await XInput.promises.enable(false); //no interrupt->ok
-/*  
-await XInput.promises.rumble();
-setTimeout(async()=>{
-  await XInput.promises.rumble({forceStateWhileRumble: true});
-},1000)
-*/
